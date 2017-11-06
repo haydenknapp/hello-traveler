8fde7096b1e1325a811b9ae34e4e7a389e99c474
Hayden Knapp <haydenknapp@gmail.com>
2017-11-05 22:47:51 -0500

    added random number generator

--- single.c
+++ single.c
@@ -28,12 +28,40 @@ static void move_npcs(npc *npcs, uint64_t npcs_per_continent) {
 	}
 }
 
+/* move all of the npcs that are out of bounds, in bounds. */
+static void move_in_bounds(npc *npcs, uint64_t npcs_per_continent) {
+	for (uint64_t i = 0; i < npcs_per_continent; ++i) {
+		if (npcs[i].x > 1.0) npcs[i].x = 1.0;
+		if (npcs[i].x < 0.0) npcs[i].x = 0.0;		
+		if (npcs[i].y > 1.0) npcs[i].y = 1.0;
+		if (npcs[i].y < 0.0) npcs[i].y = 0.0;
+	}
+}
+
 /* Assign every npc a random x, y and direction. Set inter to 0 */
 static void fill_continent(npc *npcs, uint64_t npcs_per_continent) {
 	for (uint64_t i = 0; i < npcs_per_continent; ++i) {
 		npcs[i].x = (float) rand() / (float) RAND_MAX;
 		npcs[i].y = (float) rand() / (float) RAND_MAX;
 		npcs[i].dir = (float) rand() / (float) RAND_MAX * 360.0f;
+	}
+}
+
+#define DEC 1000
+
+/* return a random float from 0.0 to 360.0 based on the given long ints. */
+static inline float get_random_float_360(uint64_t one, uint64_t two) {
+	uint64_t prod = one * two;
+	return (double)(prod % (360 * DEC)) / DEC;
+}
+
+/* If one of the x or y values of the npc is 0.0 or 1.0, then the npc must get a new direction. */
+static void give_new_dir(npc *npcs, uint64_t npcs_per_continent) {
+	for (uint64_t i = 0; i < npcs_per_continent; ++i) {
+		if (npcs[i].x == 1.0) npcs[i].dir = get_random_float_360(i, 321);
+		if (npcs[i].x == 0.0) npcs[i].dir = get_random_float_360(i, 321);
+		if (npcs[i].y == 1.0) npcs[i].dir = get_random_float_360(i, 321);
+		if (npcs[i].y == 0.0) npcs[i].dir = get_random_float_360(i, 321);
 	}
 }
 
@@ -72,7 +100,7 @@ uint64_t start_single(uint64_t seed, uint16_t num_continents, uint64_t npcs_per_
 
 #define PREC 10000.0
 
-uint8_t float_eq(float one, float two) {
+uint8_t feq(float one, float two) {
 	if((uint32_t)(one * PREC) == (uint32_t)(two * PREC)) {
 		return 1;
 	}
@@ -99,7 +127,7 @@ void test_correct_npc(uint64_t len, npc *npcs, uint8_t vol) {
 }
 
 void test_fill_continent() {
-	printf("\nTesting filling continents.\n");
+	printf("Testing filling continents.\n");
 	/* test a small continent */
 	uint64_t len = 16;
 	npc *npcs = (npc*) malloc(len * sizeof(npc));
@@ -112,6 +140,7 @@ void test_fill_continent() {
 	fill_continent(npcs, len);
 	test_correct_npc(len, npcs, QUIET);
 	free(npcs);
+	printf("Done testing filling continents.\n");
 }
 
 
@@ -142,15 +171,117 @@ void test_move_npcs() {
 
 	for (uint8_t i1 = 0, i2 = 0; i1 < 6; ++i1) {
 		printf("resx: %.8f npcx: %.8f\tresy %.8f npcy: %.8f\n", res[i2], npcs[i1].x, res[i2 + 1], npcs[i1].y);
-		assert(float_eq(npcs[i1].x, res[i2++]));
-		assert(float_eq(npcs[i1].y, res[i2++]));
+		assert(feq(npcs[i1].x, res[i2++]));
+		assert(feq(npcs[i1].y, res[i2++]));
 	}
+	printf("Done testing moving NPCs.\n");
+}
+
+void test_move_in_bounds() {
+	printf("\nTesting move back into bounds\n");
+	uint64_t n = 13;
+	npc npcs[n];
+	/* only x is negative */
+	npcs[0].x = -0.001; npcs[0].y = 0.5;
+	npcs[1].x = -654645.3; npcs[1].y = 1.0;
+	/* only y is negative */
+	npcs[2].x = 0.3; npcs[2].y = -0.3;
+	npcs[3].x = 0.9; npcs[3].y = -0.99999;
+	/* both are negative */	
+	npcs[4].x = -0.8; npcs[4].y = -0.3;
+	npcs[5].x = -0.01; npcs[5].y = -0.1;
+	/* only x is over 1.0 */
+	npcs[6].x = 1.00001; npcs[6].y = 0.3;
+	npcs[7].x = 3.000; npcs[7].y = 0.999;
+	/* only y is over 1.0 */
+	npcs[8].x = 0.000; npcs[8].y = 1.00001;
+	npcs[9].x = 0.999; npcs[9].y = 4.0000;
+	/* both above 1.0 */
+	npcs[10].x = 1.0001; npcs[10].y = 1.0001;
+	/* x neg y over 1.0 */
+	npcs[11].x = -0.0001; npcs[11].y = 1.0001;
+	/* x over 1.0 y neg */
+	npcs[12].x = 1.001; npcs[12].y = -0.001;
+
+	move_in_bounds(npcs, n);
+
+	/* feq is needed because of floating point errors. */
+	assert(npcs[0].x == 0.0);
+	assert(npcs[0].y == 0.5);
+	assert(npcs[1].x == 0.0);
+	assert(npcs[1].y == 1.0);
+	assert(feq(npcs[2].x, 0.3));
+	assert(npcs[2].y == 0.0);
+	assert(feq(npcs[3].x, 0.9));
+	assert(npcs[3].y == 0.0);
+	assert(npcs[4].x == 0.0);
+	assert(npcs[4].y == 0.0);
+	assert(npcs[5].x == 0.0);
+	assert(npcs[5].y == 0.0);
+	assert(npcs[6].x == 1.0);
+	assert(feq(npcs[6].y, 0.3));
+	assert(npcs[7].x == 1.0);
+	assert(feq(npcs[7].y, 0.999));
+	assert(npcs[8].x == 0.000);
+	assert(npcs[8].y == 1.0);
+	assert(feq(npcs[9].x, 0.999));
+	assert(npcs[9].y == 1.0);
+	assert(npcs[10].x == 1.0);
+	assert(npcs[10].y == 1.0);
+	assert(npcs[11].x == 0.0);
+	assert(npcs[11].y == 1.0);
+	assert(npcs[12].x == 1.0);
+	assert(npcs[12].y == 0.0);	
+	printf("Done testing move into bounds.\n");
+}
+
+void test_get_random_float_360() {
+	printf("\nStarting float 360 tests\n");
+	uint64_t n = 8;
+	uint64_t num = 32123;
+	float ra;
+	for (uint64_t i = 0; i < n; ++i) {
+		ra = get_random_float_360(i, num);
+		printf("Random float for %lu and %lu is: %f\n", i, num, ra);
+		assert(ra <= 360.0 && ra >= 0.0);
+	}
+
+	printf("\nFloat 360 tests are completed.\n");
+}
+
+void test_give_new_dir() {
+	printf("\nTest giving new directions.\n");
+	uint64_t n = 5;
+	npc npcs[n];
+	npcs[0].x = 0.0; npcs[0].y = 0.5; npcs[0].dir = 8.125;
+	npcs[1].x = 0.6; npcs[1].y = 1.0; npcs[1].dir = 32.0;
+	npcs[2].x = 1.0; npcs[2].y = 0.0; npcs[2].dir = 256.0;
+	npcs[3].x = 0.0; npcs[3].y = 1.0; npcs[3].dir = 128.0;
+	npcs[4].x = 0.4; npcs[4].y = 0.1; npcs[4].dir = 256.0;
+
+	give_new_dir(npcs, n);
+
+	printf("The new direction of npcs at %d is %f\n", 0, npcs[0].dir);
+	assert(npcs[0].dir != 8.125);
+	printf("The new direction of npcs at %d is %f\n", 1, npcs[1].dir);
+	assert(npcs[1].dir != 32.0);
+	printf("The new direction of npcs at %d is %f\n", 2, npcs[2].dir);
+	assert(npcs[2].dir != 256.0);
+	printf("The new direction of npcs at %d is %f\n", 3, npcs[3].dir);
+	assert(npcs[3].dir != 128.0);
+	printf("This value should not change.\n");
+	assert(npcs[4].dir == 256.0);
+
+	printf("\nTest giving new directions completed.\n");
 }
 
 int main() {
 	srand(time(NULL));
 	test_fill_continent();
 	test_move_npcs();
+	test_move_in_bounds();
+	test_get_random_float_360();
+	test_give_new_dir();
 }
 
 #endif
